c69c35aac45191962b10935978a7d8f2742638aa
AlCast <alvaro.castelan.21@gmail.com>
2018-07-17 21:04:31 -0500

    added routes and started setup for pages

--- client/src/App.js
+++ client/src/App.js
@@ -2,29 +2,32 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from "axios";
+import Wrapper from "./components/Wrapper";
+import {Router, Route} from "react-router-dom";
+import history from "./history";
+import Home from "./pages/Home";
+import Survey from "./pages/Survey";
+import Results from "./pages/Results";
+
+
 class App extends Component {
-  getGetRequest(){
+  // getGetRequest(){
    
-    axios.get("/api/database").then(res => {
-     console.log(res); 
-       	   })
-    	 }
+  //   axios.get("/api/database").then(res => {
+  //    console.log(res); 
+  //      	   })
+  //   	 }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div>
-          <button onClick={this.getGetRequest}>
-          GET TEST
-          </button>
-        </div>
-      </div>
+      <Wrapper>
+        <Router history={history}>
+          <div>
+            <Route exact path = "/" component={Home}/>
+            <Route exact path = "/survey" component={Survey}/>
+            <Route path = "/survey/results" component={Results}/>
+          </div>
+        </Router>
+      </Wrapper>
     );
   }
 }
---
+++ client/src/pages/Survey/survey.js
@@ -0,0 +1,32 @@
+import React, {Component} from "react"; 
+
+class Survey extends Component {
+
+
+
+
+
+render(){
+return(
+
+<div> Survey Page </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+)
+
+}
+
+
+}
+export default Survey; 
