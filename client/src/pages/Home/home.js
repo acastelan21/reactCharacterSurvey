7ff32eb084b57e727da760e7744825dc43d2d714
AlCast <alvaro.castelan.21@gmail.com>
2018-07-30 23:17:35 -0500

    added pictures

--- client/src/pages/Home/home.js
+++ client/src/pages/Home/home.js
@@ -12,10 +12,17 @@ return(
     <h1 className="display-4">Which character from "The Office and/or "Parks and Rec" are you most like?</h1>
     <p className="lead">Click "Take Survey" below and find out! </p>
     <hr className="my-4"/>
-   
-    <a className="btn btn-primary btn-lg" href="/survey" role="button">Take Survey</a>
+    <div className="row justify-content-center">
+    <div className="col-md-auto">
+    <img src={require("../../images/theoffice2.jpg")} alt="the office poster"/>
     </div>
-
+    <div className="col-md-auto"><a className="btn btn-primary btn-lg" href="/survey" role="button">Take Survey</a></div>
+    <div className="col-md-auto"><img src={require("../../images/parksandrec2.jpg") } alt ="parks and rec poster"/></div>
+   
+    
+    </div>
+    
+</div>
 </div>
 
 
