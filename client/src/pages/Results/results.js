7c75afc2ca9d0fb4559183f7bff01f9816f7c5a0
AlCast <alvaro.castelan.21@gmail.com>
2018-07-30 22:46:21 -0500

    minor change

--- client/src/pages/Results/results.js
+++ client/src/pages/Results/results.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import {connect} from "react-redux";
 import axios from "axios";
-
+import "./results.css";
 class Results extends Component{
     constructor(props){
         super(props);
